bf560994312ea8cc2ebfd7af7e75b978f2e1d4c2
quinnyo <3379314+quinnyo@users.noreply.github.com>
2024-03-23 04:20:49 +1000

    Add memcpy_terminated

--- src/mem.asm
+++ src/mem.asm
@@ -14,3 +14,22 @@ memcpy::
 	or b
 	jr nz, _memcpy_loop
 	ret
+
+
+; Copy a contiguous block of bytes from `src` to `dest`.
+; The block ends at the first instance of `terminator` read from the `src` region.
+; @NOTE: Does **not** copy the terminator byte.
+; @param HL: dest
+; @param DE: src
+; @param C: terminator
+; @return DE: last source address read
+; @return HL: end of written block
+; @mut: AF, DE, HL
+memcpy_terminated::
+:
+	ld a, [de]
+	cp c
+	ret z
+	inc de
+	ld [hl+], a
+	jr :-
